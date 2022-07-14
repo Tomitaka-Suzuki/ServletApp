3ebf8f504dad49cd6150bf4407ae89492bc25072
Tomitaka Suzuki <t.suzuki.sys21@morijyobi.ac.jp>
2022-07-14 10:19:59 +0900

    mypageServletの更新

--- Servlet01/src/servlet/mypageServlet.java
+++ Servlet01/src/servlet/mypageServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,15 +13,16 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/mypageServlet")
 public class mypageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    
-	
+
+
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher dispatcher =
+				request.getRequestDispatcher("WEB-INF/jsp/u_mypage.jsp");
+		dispatcher.forward(request, response);
 	}
 
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
