df755dc9a78e6ce375e7894e7460dab8773f0643
Motokazu Ito <m.itou.sys21@morijyobi.ac.jp>
2022-08-25 10:32:45 +0900

    新規登録ページを作成しますた

--- Servlet01/src/scopedata/Account.java
+++ Servlet01/src/scopedata/Account.java
@@ -5,18 +5,17 @@ import java.io.Serializable;
 public class Account implements Serializable{
 	private String id;
 	private String pass;
-	
+
 	public Account() {}
 	public Account( String _id, String _pass) {
 		id = _id;
 		pass = _pass;
 	}
-	
+
 	public String getId() {
 		return id;
 	}
 	public String getPass() {
 		return pass;
 	}
-
 }
